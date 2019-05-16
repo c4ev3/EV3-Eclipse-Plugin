7873637e690acbce6bc38cbf75759130ac4134e0
Daniel Mueller <daniel@dnml.de>
2019-05-16 22:43:31 +0200

    Add template with with for recent API
    Changed the template code to use TermPrintf/TermPrintln and
    removed Init/Free functions.
    Added template HelloWorld_EV3_C_Project_Old that uses the old code.

--- de.hab.ev3plugin/templates/projecttemplates/HelloWorld_EV3_C_Project/src/Basename.c
+++ de.hab.ev3plugin/templates/projecttemplates/HelloWorld_EV3_C_Project/src/Basename.c
@@ -9,14 +9,18 @@
 
 int main(void)
 {
+
+	// INFO This code template works only with recent versions of the API. If TermPrintln is not found,
+	//      please update the API or use the "Hello World EV3 Project Old API" as template.
+
+
 	//TODO Place here your variables
 
 
 	//TODO Place here your code
-	InitEV3();
-	LcdPrintf(1, "Hello World!\n");
-	Wait(SEC_2);
+	TermPrintln("Hello World!");
+	TermPrintf("Press ENTER to exit");
+	ButtonWaitForPress(BUTTON_ID_ENTER);
 
-	FreeEV3();
 	return 0;
 }
